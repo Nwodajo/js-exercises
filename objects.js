517f3705340cbffc892aeb9f1ddea4c1ab94635d
Nwodajo <120424900+Nwodajo@users.noreply.github.com>
2025-10-09 21:54:15 -0700

    Complate  object exericises

--- objects.js
+++ objects.js
@@ -9,12 +9,12 @@ Add the property surname with the value Smith.
 Change the value of the name to Pete.
 Remove the property name from the object.
 */ 
-let user = {};
-user.name = "Jone";
-user.surname = "Smith";
-user.name ="Pete";
-delete user.name;
-console.log(user);
+// let user = {};
+// user.name = "Jone";
+// user.surname = "Smith";
+// user.name ="Pete";
+// delete user.name;
+// console.log(user);
 
 
 /*
@@ -32,9 +32,89 @@ schedule["8:30"] = "get up";
 
 alert( isEmpty(schedule) ); // false
 Open a sandbox with tests.
+*/
+
+// function isEmpty(obj) {
+//     for( let key in obj){
+//         return  false;
+//     }
+//     return true;
+// }
+//  let schedule = {};
+//  console.log(isEmpty(schedule)); // true
+
+//  schedule["8:30"] = "get up";
+//  console.log(isEmpty(schedule)); // false
 
 
+ /*
+We have an object storing salaries of our team:
 
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+}
+Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above.
 
+If salaries is empty, then the result must be 
+*/ 
+let  salaries ={
+    Jhon: 100,
+    Ann: 160,
+  Pete: 130
+};
+let sum = 0;
+for (let key in salaries){
+    sum += salaries[key];
+}
+console.log(sum);
+/*
+
+Create a function multiplyNumeric(obj) that multiplies all numeric property values of obj by 2.
+
+For instance:
+
+// before the call
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+multiplyNumeric(menu);
+
+// after the call
+menu = {
+  width: 400,
+  height: 600,
+  title: "My menu"
+};
+Please note that multiplyNumeric does not need to return anything. It should modify the object in-place.
 
 */
+
+
+
+
+
+
+
+
+
+    function multiplyNumeric(obj){
+        for (let key in obj){
+            if (typeof obj[key] === 'number');{
+                obj[key] *= 2;
+            }
+        }
+
+    }
+    //*
+  let   menu = {
+  width: 400,
+  height: 600,
+  title: "My menu"
+  };
+  multiplyNumeric(menu);
+  console.log(menu);
