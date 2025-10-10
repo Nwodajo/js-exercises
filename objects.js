7af7c7fba8ccd4824d0be6b4b19830abc231596f
Nwodajo <120424900+Nwodajo@users.noreply.github.com>
2025-10-09 20:26:57 -0700

    Add first objects

---
+++ objects.js
@@ -0,0 +1,40 @@
+/*
+
+
+Write the code, one line for each action:
+
+Create an empty object user.
+Add the property name with the value John.
+Add the property surname with the value Smith.
+Change the value of the name to Pete.
+Remove the property name from the object.
+*/ 
+let user = {};
+user.name = "Jone";
+user.surname = "Smith";
+user.name ="Pete";
+delete user.name;
+console.log(user);
+
+
+/*
+Check for emptiness
+importance: 5
+Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.
+
+Should work like that:
+
+let schedule = {};
+
+alert( isEmpty(schedule) ); // true
+
+schedule["8:30"] = "get up";
+
+alert( isEmpty(schedule) ); // false
+Open a sandbox with tests.
+
+
+
+
+
+*/
